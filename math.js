56d5a519e7a2f78602ff335c56fe326207372d01
Sukkarin-Dev <sukkarin.aun@gistda.or.th>
2024-06-28 13:09:10 +0700

    Create math.js

---
+++ math.js
@@ -0,0 +1,17 @@
+function sum(a, b) {
+    return a + b;
+}
+
+function multipiy(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    if (b === 0) {
+        throw new Error('Cannot divide by zero')
+    }
+    return a/b;
+}
+
+module.exports = {sum,multipiy,divide}
+
